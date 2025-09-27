db6b3af66a70d486e3d8a690c699b4924608e70b
AbdessamadChaoui <abdessamad.chaoui@student.forsbergsskola.se>
2025-09-27 21:32:54 +0200

    E9Operators Seconds Calculator
    This is a calculator of seconds, it converts seconds into days, hours and minutes.
    Eventually it displays this format : D.H:M:S.

---
+++ E9Operators/Program.cs
@@ -0,0 +1,29 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("SECONDS CALCULATOR \nGive me a number of seconds : \n");
+int totalSec = Convert.ToInt32(Console.ReadLine());
+
+int daySec = 86400;
+int hourSec = 3600;
+int minSec = 60;
+
+int days = totalSec / daySec;
+float daysInPieces = (float)totalSec / daySec;
+int modulol =  totalSec % daySec;
+
+int hours = modulol / hourSec;
+modulol = modulol % hourSec;
+
+int minutes = modulol / minSec;
+
+int seconds = modulol % minSec;
+
+Console.WriteLine("Seconds: " + seconds);
+Console.WriteLine("Minutes: " + minutes);
+Console.WriteLine("Hours: " + hours);
+Console.WriteLine("Days: " + days);
+
+Console.WriteLine($"{days}.{hours}:{minutes}:{seconds}");
+Console.WriteLine("In total, that's " + daysInPieces + " Days.");
+
+
